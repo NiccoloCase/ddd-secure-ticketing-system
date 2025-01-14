7f74012ca7391c5cd7cbcf913d75ea9bed22163b
niccolocaselli <caselli.niccolo.developer@gmail.com>
2025-01-14 13:32:42 +0100

    implemented password manager

---
+++ src/main/java/org/swe/helpers/PasswordManager.java
@@ -0,0 +1,17 @@
+package org.swe.helpers;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordManager {
+
+
+    public static String hashPassword(String password) {
+        String salt = BCrypt.gensalt();
+        return BCrypt.hashpw(password, salt);
+    }
+
+
+    public static boolean checkPassword(String password, String hashedPassword) {
+        return BCrypt.checkpw(password, hashedPassword);
+    }
+}

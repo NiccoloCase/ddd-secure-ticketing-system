75fb5c1f1b5823b5101feeb35f02d8cf7b21a08e
LautaroCavichia <lautarocavichia@gmail.com>
2025-01-18 16:18:28 +0100

    DAO fix

--- src/main/java/org/swe/core/DAO/EventDAO.java
+++ src/main/java/org/swe/core/DAO/EventDAO.java
@@ -1,13 +1,14 @@
 package org.swe.core.DAO;
 
-import org.swe.model.Event;
 import java.util.ArrayList;
+import java.util.Date;
+
+import org.swe.model.Event;
 
 public interface EventDAO {
-     Event getEvent(int id);
-     ArrayList<Event> getAllEvents();
-     boolean addEvent(Event event);
-     boolean updateEvent(Event event);
-     boolean deleteEvent(int id);
-     
+    Event getEventById(int id);
+    ArrayList<Event> getAllEvents();
+    Event createEvent(String title, String description, Date date, int ticketsAvailable, double ticketPrice);
+    boolean updateEvent(int id, String title, String description, Date date, int ticketsAvailable, double ticketPrice);
+    boolean deleteEvent(int id);
 }
